b0cbb3a7efb562cd87b882ac351b8001201d1c67
shenxx <shenxiangxiang@gmail.com>
2021-09-20 23:40:29 +0800

    增加一种直接brute force下一个随机数的方法

--- mersenne_cracker.py
+++ mersenne_cracker.py
@@ -5,6 +5,12 @@ from z3 import *
 class MersenneCracker:
     """[summary]
     https://nayak.io/posts/mersenne_twister/
+
+    观察到的随机数序列需要大于或者等于n
+
+    有两种使用方式：
+    1. 已知连续的n个随机数且没有发生twist -> 使用crack_state (注意代码中是取前n个随机数)
+    2. 已知连续的n个随机数但不确定twist的情况 -> 使用_brute_force_closest_twist_index或者predict_next_state
     """    
     def __init__(self, variant = "mt19937", parameters = []):
         if variant == "mt19937":  # 32-bit version of original twister
@@ -66,8 +72,9 @@ class MersenneCracker:
             self.f = parameters[12]
         else:
             raise NotImplementedError
-        self.lower_mask = (1 << self.r) - 1
-        self.upper_mask = 1 << self.r
+        self.lower_mask = int("1"*(self.r), 2)
+        self.upper_mask = int("1"*(self.w-self.r)+"0"*self.r, 2)
+
 
     def crack_state(self, outputs):
         self.original_state = [0] * self.n
@@ -79,6 +86,31 @@ class MersenneCracker:
         self.original_state = map(self._untemper, outputs)
         return list(self.original_state)
 
+    def untemper_right(self, n, shift):
+        i = 0
+        while i * shift < self.w:
+            new_mask = n & (((((1 << self.w) - 1) << (self.w - shift)) & ((1 << self.w) - 1)) >> (shift * i))
+            n ^= new_mask >> shift
+            i += 1
+        return n
+
+    def untemper_right_mask(self, n, shift, mask):
+        i = 0
+        while i * shift < self.w:
+            new_mask = n & (((((1 << self.w) - 1) << (self.w - shift)) & ((1 << self.w) - 1)) >> (shift * i))
+            new_mask >>= shift
+            n ^= new_mask & mask
+            i += 1
+        return n
+
+    def untemper_left(self, n, shift, mask):
+        i = 0
+        while i * shift < self.w:
+            new_mask = n & ((((1 << self.w) - 1) >> (self.w - shift)) << (shift * i))
+            new_mask <<= shift
+            n ^= new_mask & mask
+            i += 1
+        return n
 
     def untwist(self, state):  # returns the past state before a twist
         state = list(state)
@@ -94,7 +126,21 @@ class MersenneCracker:
             state[i] = shifted ^ (temp << 1) & self.lower_mask
         return state
 
+
+    def untemper(self, observed_values):
+        """returns untempered values of observed
+        """        
+        return list(map(self._untemper, observed_values))
+
     def _untemper(self, single_observed_value):
+        x = single_observed_value
+        y = self.untemper_right(x, self.l)
+        y = self.untemper_left(y, self.t, self.c)
+        y = self.untemper_left(y, self.s, self.b)
+        y = self.untemper_right_mask(y, self.u, self.d)
+        return y
+
+    def _untemper_with_z3(self, single_observed_value):
         """https://www.schutzwerk.com/en/43/posts/attacking_a_random_number_generator/
         x = self.state[self.index]
         y1 = x ^ ((x >> self.u) & self.d) 
@@ -133,33 +179,111 @@ class MersenneCracker:
         s.add(equations)
         assert s.check() == sat
         return s.model()[x].as_long()
+    
 
+    def _predict_next_state(self, x, closest_twist_index):
+        assert len(x) >= self.n
+        second_closest_twist_index = closest_twist_index - self.n
+        k_plus_n = len(x) # next state
+        k = k_plus_n -self.n
+        k_plus_1 = (k_plus_n -self.n + 1 - closest_twist_index) % self.n + second_closest_twist_index
+        k_plus_m = (k_plus_n -self.n + self.m - closest_twist_index) % self.n + second_closest_twist_index
+        x__k = x[k]
+        x__k_plus_1 = x[k_plus_1]
+        x__k_plus_m = x[k_plus_m]
+
+        # twist: x[k+n] only depends on x[k], x[k+m], x[k+l]
+        temp = (x__k & self.upper_mask) + (x__k_plus_1 & self.lower_mask)
+        shifted = temp >> 1
+        if temp % 2: # if the lowest order bit of x==1 -> xor self.a
+            shifted ^= self.a
+        x__k_plus_n = x__k_plus_m ^ shifted
+        return self._temper(x__k_plus_n)
+
+
+    def _brute_force_closest_twist_index(self, observed_values, x):
+        """[summary]
+
+        x[k+n] only depends on x[k], x[k+m], x[k+l]
+
+        Assume observed_values[:- (len(observed_values) - self.n)] as unknown, brute force twist index
+        """
+        assert len(observed_values) > self.n
+        assert len(observed_values) == len(x)
+
+        best_guesses = [] # brute force twist index
+        best_guess_right_count = 0
+
+        for tmp_twist_index in range(self.n, len(observed_values)):
+            
+            tmp_best_guess_right_count = 0
+            for guess_index in range(0, len(observed_values) - self.n - 1):
+                try:
+                    y_hat = self._predict_next_state(x[:-1-guess_index], tmp_twist_index)
+                    # print(tmp_twist_index,guess_index, y_hat, observed_values[-1-guess_index])
+                    if y_hat == observed_values[-1-guess_index]:
+                        tmp_best_guess_right_count += 1
+                    else:
+                        break
+                except:
+                    pass
+
+            if tmp_best_guess_right_count > best_guess_right_count:
+                best_guess_right_count = tmp_best_guess_right_count
+                best_guesses.clear()
+                best_guesses.append(tmp_twist_index)
+            elif tmp_best_guess_right_count == best_guess_right_count:
+                best_guesses.append(tmp_twist_index)
+        
+        return list(best_guesses)
+
+    def _temper(self, x):
+        """Tempered representation of internal states
+        where x is the next value from the series, 
+        """        
+        y = x ^ ((x >> self.u) & self.d) 
+        y ^= ((y << self.s) & self.b)
+        y ^= ((y << self.t) & self.c)
+        y ^= (y >> self.l)
+        return self._fixed_int(y) # lowest w bits of y
+ 
+    def _fixed_int(self, n):  # 
+        """truncate int to lowest w bits
+        ((1 << self.w) - 1) = 0b11111111111111111111111111111111 if w=32
+        """        
+        return ((1 << self.w) - 1) & n
+
+    def predict_next_state(self, observed_values):
+        predictions = set()
+        x = self.untemper(observed_values) # states
+        guessed_closest_twist_index_list = self._brute_force_closest_twist_index(observed_values, x)
+        for guessed_closest_twist_index in guessed_closest_twist_index_list:
+            predictions.add(self._predict_next_state(x, guessed_closest_twist_index))
+        return list(predictions)
 
 if __name__ == "__main__":
-    # this can also be used to crack floats if you scale and round them first
-    random_32 = MersenneTwister()
-    old_state_32 = random_32.getstate()
-    outputs_32 = [random_32.random_integer() for _ in range(624)]
-    cracker_32 = MersenneCracker()
-    new_state_32 = cracker_32.crack_state(outputs_32)
-    random_32.setstate([new_state_32, 0])  # start at beginning of state
-    assert outputs_32 == [random_32.random_integer() for _ in range(624)]
-    print("32-bit Successfully Cracked")
-    assert cracker_32.untwist(new_state_32)[1:] == old_state_32[0][1:]  # seed may be different
-    print("32-bit Successfully Reversed")
-
-    random_32_alt = MersenneTwister(variant = "mt11213b")
-    outputs_32_alt = [random_32_alt.random_integer() for _ in range(624)]
-    cracker_32_alt = MersenneCracker(variant = "mt11213b")
-    new_state_32_alt = cracker_32_alt.crack_state(outputs_32_alt)
-    random_32_alt.setstate([new_state_32_alt, 0])  # start at beginning of state
-    assert outputs_32_alt == [random_32_alt.random_integer() for _ in range(624)]
-    print("32-bit Alt Successfully Cracked")
-
+    # 第一种使用场景: 已知连续的n个随机数且没有发生twist -> 使用crack_state
+    # 对于随机数个数超过n的情况, 默认是取前n个 (line 83)
     random_64 = MersenneTwister(variant = "mt19937_64")
-    outputs_64 = [random_64.random_integer() for _ in range(312)]
+    outputs_64 = [random_64.random_integer() for _ in range(1024)]
     cracker_64 = MersenneCracker(variant = "mt19937_64")
-    new_state_64 = cracker_64.crack_state(outputs_64)
+
+    # 假设我们只观察到了前n个随机数, 后面的随机数未观察到
+    observed_values = outputs_64[:random_64.n]
+    new_state_64 = cracker_64.crack_state(observed_values)
     random_64.setstate([new_state_64, 0])  # start at beginning of state
-    assert outputs_64 == [random_64.random_integer() for _ in range(312)]
-    print("64-bit Successfully Cracked")
+    assert outputs_64 == [random_64.random_integer() for _ in range(1024)]
+    print("64-bit Case 1 Successfully Cracked")
+
+    # ===================================================
+    # 第二种使用场景: 已知连续的n个随机数但不确定twist的情况 -> 使用_brute_force_closest_twist_index或者predict_next_state
+    observed_values = []
+    with open("520.txt", "r") as f:
+        for line in f.readlines():
+            num = int(line)
+            observed_values.append(num)
+    cracker_64 = MersenneCracker(variant = "mt19937_64")
+    guessed_random_numbers = cracker_64.predict_next_state(observed_values)
+    assert 310574417605022995 in guessed_random_numbers
+    print("64-bit Case 2 Successfully Cracked")
+
